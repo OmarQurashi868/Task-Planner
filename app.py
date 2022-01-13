593ee64758a3a2f2cfac69ea491b12104b9203ce
Omar Kamal <OmarQurashi868@gmail.com>
2022-01-13 19:39:54 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,10 @@ from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 
 
-# do not cache responses
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
 
 # configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
